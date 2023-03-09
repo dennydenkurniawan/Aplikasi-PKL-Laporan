e3a1750f81cd76d3ef201a0a15f2f2f751c45a7b
dennydenkurniawan <89555119+dennydenkurniawan@users.noreply.github.com>
2023-03-09 10:15:18 +0800

    Upload File #1

---
+++ laker_pa/login.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Laker PA | Log in</title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="app/plugins/fontawesome-free/css/all.min.css">
+  <!-- icheck bootstrap -->
+  <link rel="stylesheet" href="app/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="app/dist/css/adminlte.min.css">
+  <!-- SweetAlert2 -->
+  <link rel="stylesheet" href="app/plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css">
+</head>
+<body class="hold-transition login-page">
+    <div class="login-box">
+      <!-- /.login-logo -->
+      <div class="card card-outline card-primary">
+        <div class="card-header text-center">
+          <a href="../../index2.html" class="h1"><b>Laker</b>PA</a>
+          <p class="h6">Aplikasi Pelaporan Tindak Kekerasan Terhadap Perempuan dan Anak</p>
+        </div>
+        <div class="card-body">
+          <p class="login-box-msg">Silakan Login Terlebih Dahulu</p>
+
+          <form action="conf/autentikasi_.php" method="post">
+            <div class="input-group mb-3">
+              <input type="text" class="form-control" placeholder="Username" name="username">
+              <div class="input-group-append">
+                <div class="input-group-text">
+                  <span class="fas fa-user-alt"></span>
+                </div>
+              </div>
+            </div>
+            <div class="input-group mb-3">
+              <input type="password" class="form-control" placeholder="Password" name="password">
+              <div class="input-group-append">
+                <div class="input-group-text">
+                  <span class="fas fa-lock"></span>
+                </div>
+              </div>
+            </div>
+            <div class="input-group mb-3">
+              <a href="registrasi.php" class="text-center">Registrasi Akun Baru</a>
+            </div>
+            <div class="row text-center">
+              <div class="col-5 mx-auto">
+                <button type="submit" class="btn btn-danger btn-block">Login</button>
+              </div>
+            </div>
+          </form>
+
+          <!-- <div class="social-auth-links text-center mt-2 mb-3">
+            <a href="#" class="btn btn-block btn-primary">
+              <i class="fab fa-facebook mr-2"></i> Sign in using Facebook
+            </a>
+            <a href="#" class="btn btn-block btn-danger">
+              <i class="fab fa-google-plus mr-2"></i> Sign in using Google+
+            </a>
+          </div> -->
+          <!-- /.social-auth-links -->
+
+          <!-- <p class="mb-1">
+            <a href="forgot-password.html">I forgot my password</a>
+          </p>
+          <p class="mb-0">
+            <a href="register.html" class="text-center">Register a new membership</a>
+          </p> -->
+        </div>
+        <!-- /.card-body -->
+      </div>
+      <!-- /.card -->
+    </div>
+  <!-- /.login-box -->
+
+<!-- jQuery -->
+<script src="app/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="app/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="app/dist/js/adminlte.min.js"></script>
+<!-- SweetAlert2 -->
+<script src="app/plugins/sweetalert2/sweetalert2.min.js"></script>
+</body>
+<style>
+  body {
+    margin: 0;
+    padding: 0;
+    height: 100%;
+    background-image: url("app/portal/bg.jpg");
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+    background-attachment: fixed;
+
+}
+</style>
+
+<?php
+// Membuat Notifikasi
+if(isset ($_GET['error'])){
+$x = ( $_GET['error']);
+  if($x==1){
+    echo "
+    <script>
+      var Toast = Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 3000
+      });
+      Toast.fire({
+        icon: 'error',
+        title: 'Login Gagal'
+      })
+    </script>";
+  }
+  else if($x==2){
+    echo "
+    <script>
+      var Toast = Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 3000
+      });
+      Toast.fire({
+        icon: 'warning',
+        title: 'Silahkan Inputkan Username & Password'
+      })
+    </script>";
+  }
+}
+?>
+
+</html>
