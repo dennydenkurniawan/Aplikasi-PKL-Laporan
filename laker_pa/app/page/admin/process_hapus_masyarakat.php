4f6d835b14365ae279fbb228fad61b863d0291de
dennydenkurniawan <89555119+dennydenkurniawan@users.noreply.github.com>
2023-03-10 08:58:17 +0800

    Update Folder Page

--- laker_pa/app/page/admin/process_hapus_masyarakat.php
+++
@@ -1,8 +0,0 @@
-<?php
-include('../../../conf/config.php');
-$id = $_GET['id'];
-
-$query = mysqli_query($koneksi, "DELETE FROM akun_masyarakat WHERE id='$id'");
-
-header('location: ../../index.php?page=data-masyarakat');
-?>
